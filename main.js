d9cbc3adf18215c0c799e1decc05f19237ce09ea
Hannah.Celemen <hcelemen@icloud.com>
2022-07-26 16:50:21 -0600

    Save a card in data model and show in DOM

--- main.js
+++ main.js
@@ -1,19 +1,58 @@
 // query selector below
 titleInput = document.querySelector('#title-box');
 bodyInput = document.querySelector('#body-box');
+// commentInput = document.querySelector('.card-comments');
 saveButton = document.querySelector('.user-form--button');
+ideaCardsArea = document.querySelector('.idea-cards--area');
+ideaCard = document.querySelector('.idea-card');
+
+//global variables
+let listOfBoxes = [];
 
 
 
 //event listeners below
 saveButton.addEventListener('click', saveUserInfo);
+ideaCardsArea.addEventListener('click', targetElement);
+
+// the parent element is ideaCardsArea
+// the children elements are the ideaCard
+// we need a function that will add an ideaCard to the parent
+// the ideaCard is the instance of Idea that's in the array
+// the function needs to iterate thru the Array and create a copy of each instance into a box
+
+function createIdeaCard() {
+  for (let i = 0; i < listOfBoxes.length; i++) {
+    ideaCardsArea.innerHTML += `<div class="idea-card">
+    <div class="card-header">
+      <img class="star-active-icon" src="./assets/star-active.svg" alt="star-active-icon">
+      <img class="delete-icon" src="./assets/delete.svg" alt="delete-icon">
+    </div>
+    <div class="card-body">
+      <p class="idea-title">${listOfBoxes[i].title}</p>
+      <p>${listOfBoxes[i].body}</p>
+    </div>
+    <div class="card-comments"><img class="plus-icon" src="./assets/comment.svg" alt="plus-icon">
+      <div>comment</div>
+    </div>
+  </div>`;
+  }
+}
+
+
 
 function saveUserInfo(event) {
   event.preventDefault();
 
-  var ideaBox = new Idea(titleInput.value, bodyInput.value);
+  let ideaBox = new Idea(titleInput.value, bodyInput.value);
 
-  return ideaBox;
+  listOfBoxes.push(ideaBox);
+  console.log(listOfBoxes);
+
+  createIdeaCard();
 }
 
+function targetElement(event) {
+  event.target
+}
 
