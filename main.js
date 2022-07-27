78584dfc2c1b7f71834bf4f1de1dc860f6ace23a
Catalyst <103901063+Catalyst4Change@users.noreply.github.com>
2022-07-27 10:01:25 -0600

    Bugfix idea card creation

--- main.js
+++ main.js
@@ -9,39 +9,42 @@ ideaCard = document.querySelector('.idea-card');
 //global variables
 let listOfBoxes = [];
 
-
-
 //event listeners below
 saveButton.addEventListener('click', saveUserInfo);
 ideaCardsArea.addEventListener('click', targetElement);
 
 function createIdeaCard() {
-  for (let i = 0; i < listOfBoxes.length; i++) {
     ideaCardsArea.innerHTML += `<div class="idea-card">
     <div class="card-header">
       <img class="star-active-icon" src="./assets/star-active.svg" alt="star-active-icon">
       <img class="delete-icon" src="./assets/delete.svg" alt="delete-icon">
     </div>
     <div class="card-body">
-      <p class="idea-title">${listOfBoxes[i].title}</p>
-      <p>${listOfBoxes[i].body}</p>
+      <p class="idea-title">${listOfBoxes[0].title}</p>
+      <p>${listOfBoxes[0].body}</p>
     </div>
     <div class="card-comments"><img class="plus-icon" src="./assets/comment.svg" alt="plus-icon">
       <div>comment</div>
     </div>
   </div>`;
-  }
 }
+
 
 function saveUserInfo(event) {
   event.preventDefault();
 
   let ideaBox = new Idea(titleInput.value, bodyInput.value);
 
-  listOfBoxes.push(ideaBox);
-  console.log(listOfBoxes);
+  if (titleInput.value && bodyInput.value) {
+    listOfBoxes.unshift(ideaBox);
+    createIdeaCard();
+    clearInputBoxes();
+  }
+}
 
-  createIdeaCard();
+function clearInputBoxes() {
+  titleInput.value = "";
+  bodyInput.value = "";
 }
 
 function targetElement(event) {
